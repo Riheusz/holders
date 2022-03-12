c5916feee5431760ea42a4e82dccc624a03e48ce
ekso to pro <80045777+eksotopro@users.noreply.github.com>
2022-03-12 15:29:17 +0100

    Update hoodsense-ui-library.lua

--- hoodsense-ui-library.lua
+++ hoodsense-ui-library.lua
@@ -14,11 +14,11 @@ local utility = {}
 -- themes
 local objects = {}
 local themes = {
-	Background = Color3.fromRGB(24, 24, 24), 
-	Glow = Color3.fromRGB(0, 0, 0), 
-	Accent = Color3.fromRGB(10, 10, 10), 
-	LightContrast = Color3.fromRGB(20, 20, 20), 
-	DarkContrast = Color3.fromRGB(14, 14, 14),  
+	Background = Color3.fromRGB(0, 0, 14),
+	Glow = Color3.fromRGB(46, 46, 255),
+	Accent = Color3.fromRGB(0, 3, 44),
+	LightContrast = Color3.fromRGB(0, 3, 44),
+	DarkContrast = Color3.fromRGB(0, 0, 30),  
 	TextColor = Color3.fromRGB(255, 255, 255)
 }
 
