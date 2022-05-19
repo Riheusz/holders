655f37152ca0c344abdbf411ca95cf1c35da9d39
ekso <80045777+eksotopro@users.noreply.github.com>
2022-05-19 03:18:05 +0200

    Update pray.lua

--- pray.lua
+++ pray.lua
@@ -1,22 +1,8 @@
-local Player = game:GetService("Players").LocalPlayer
-			local UniversalAnimation = Instance.new("Animation")
-
-			function stopTracks()
-				for _, v in next, game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):GetPlayingAnimationTracks() do
-					if (v.Animation.AnimationId:match("rbxassetid")) then
-						v:Stop()
-					end
-				end
-			end
-
-			function loadAnimation(id)
-				if UniversalAnimation.AnimationId == id then
-					stopTracks()
-					UniversalAnimation.AnimationId = "1"
-				else
-					UniversalAnimation.AnimationId = id
-					local animationTrack = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):LoadAnimation(UniversalAnimation)
-					animationTrack:Play()
-				end
-			end
-loadAnimation("rbxassetid://3487719500")
+for i,v in pairs(game:GetService('Players'):GetChildren()) do
+   Player = v.Name
+   AnimationId = "3487719500"
+   local Anim = Instance.new("Animation")
+   Anim.AnimationId = "rbxassetid://"..AnimationId
+   local k = game.Players[Player].Character.Humanoid:LoadAnimation(Anim)
+   k:Play() --Play the animation
+end
