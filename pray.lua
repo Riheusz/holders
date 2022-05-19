05a2e474869e018384b73de0c8120cc967577327
ekso <80045777+eksotopro@users.noreply.github.com>
2022-05-19 03:20:32 +0200

    Update pray.lua

--- pray.lua
+++ pray.lua
@@ -1,8 +1,7 @@
 for i,v in pairs(game:GetService('Players'):GetChildren()) do
-   Player = v.Name
    AnimationId = "3487719500"
    local Anim = Instance.new("Animation")
    Anim.AnimationId = "rbxassetid://"..AnimationId
-   local k = game.Players[Player].Character.Humanoid:LoadAnimation(Anim)
+   local k = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Anim)
    k:Play() --Play the animation
 end
