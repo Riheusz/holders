af61d9bf3aa1d3007c87c874809c3509d41078ce
ekso <80045777+eksotopro@users.noreply.github.com>
2022-06-24 20:51:10 +0200

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,7 +1,1 @@
-local function Banned(UserID, Reason)
-	if game:GetService('Players').LocalPlayer.UserId == UserID then
-		game:GetService('Players').LocalPlayer:Kick('                                Zap BANNED:                                                           '..Reason)
-	end
-end
-
-Banned(243011466, 'Reselling premium.')
+warn("[!] This link is not supported anymore.")
